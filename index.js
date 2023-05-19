bd8bc2e5d37bec73a099d806ad1995c2636561d9
Delowar Hossain <bscdelowar@yahoo.com>
2023-05-19 16:34:28 +0600

    all toy get from mongodb server done

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ const client = new MongoClient(uri, {
       // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
         const toyCollection=client.db('sportsVillageDB').collection('toys')
+
+      app.get('/toys', async(req,res)=>{
+        const cursor = toyCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+      })
+
       app.post('/toys', async(req,res)=>{
         const toy=req.body
         const result = await toyCollection.insertOne(toy);
