72382b16c98d7e2722659c3a58e10dfad6594b7f
Delowar Hossain <bscdelowar@yahoo.com>
2023-05-21 21:47:33 +0600

    final cheaking done

--- index.js
+++ index.js
@@ -9,14 +9,12 @@ const port=process.env.PORT||'5000'
 app.use(cors())
 app.use(express.json())
 
-
 app.get('/', (req, res)=>{
     res.send('Server connected')
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ow6kx3p.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
@@ -27,7 +25,7 @@ const client = new MongoClient(uri, {
   
   async function run() {
     try {
-      // Connect the client to the server	(optional starting in v4.7)
+      // Connect the client to the server
       client.connect();
         const categoryCollection=client.db('sportsVillageDB').collection('category')
         const toyCollection=client.db('sportsVillageDB').collection('toys')
@@ -35,11 +33,13 @@ const client = new MongoClient(uri, {
         const result = await categoryCollection.find().toArray();
         res.send(result)
       })
+      //Insert Toy
       app.post('/toys', async(req,res)=>{
         const toy=req.body
         const result = await toyCollection.insertOne(toy);
         res.send(result)
       })
+      //Read Toys 
       app.get('/toys', async(req,res)=>{
         const limit=20
         let query={}
@@ -49,6 +49,7 @@ const client = new MongoClient(uri, {
         const result = await toyCollection.find(query).limit(limit).sort({ price: -1 }).toArray();
         res.send(result)
       })
+
       // category wise data
       app.get('/category/:catname', async(req,res)=>{
         const catname=req.params.catname
@@ -56,6 +57,7 @@ const client = new MongoClient(uri, {
         const result = await toyCollection.findOne(query)
         res.send(result)
       })
+
       //update 
       app.get('/toys/:id', async(req,res)=>{
         const id=req.params.id
@@ -91,8 +93,8 @@ const client = new MongoClient(uri, {
         const result=await toyCollection.deleteOne(query)
         res.send(result)
       })
+
       //search 
-   
       app.get('/toySearch/:text', async(req,res)=>{
         const searchText=req.params.text;
         const result=await toyCollection.find({
@@ -100,6 +102,7 @@ const client = new MongoClient(uri, {
         ]}).toArray()
         res.send(result)
       })
+
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
